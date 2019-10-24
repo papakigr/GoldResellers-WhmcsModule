da6e6988ad336cce60f688aaf3b31e463de6e08e
Voula Troulaki <v.trulaki@usableweb.gr>
2019-10-24 11:07:17 +0300

    Change Default Langstrings for EU Country of citizenship

--- ελληνικά/lang/overrides/english.php
+++ ελληνικά/lang/overrides/english.php
@@ -4,4 +4,5 @@
 //Gr additional fields
 $_LANG['grcompanytitle'] = "Company Title (Optional)";
 $_LANG['grcompanytitledescription'] = "If the domain Registrant is a Company , please fill the company Title.";
-$_LANG['eucitizenshipdescription'] = "Registrant Citizenship.";
+$_LANG['eucitizenship'] = "Country of Citizenship";
+$_LANG['eucitizenshipdescription'] = "Registrant Country of Citizenship.";
